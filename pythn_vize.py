4d20fa2cce6e2ccef0694d4a808c9aca483e737b
ugurgl <95630501+ugurgl@users.noreply.github.com>
2021-12-11 19:14:08 +0300

    Add files via upload

---
+++ pythn_vize.py
@@ -0,0 +1,19 @@
+class Yemek:
+    yemekler = []
+    yemek = input("Yemek ismi: ")
+    yemekler.append(yemek)
+    for x in yemekler:
+        print(x)
+    
+class Tarif(Yemek):
+    tarifler = []
+    tarif = input("Tarif Gir :")
+    tarifler.append(tarif)
+    for x in tarifler:
+        print(x)
+
+
+
+
+
+    
